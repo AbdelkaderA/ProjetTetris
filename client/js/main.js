abf1034e8c53c4de440cccfe83abab080de23bb2
AbdelkaderA <58740908+AbdelkaderA@users.noreply.github.com>
2020-04-30 17:31:09 +0200

    Formation des blocs

---
+++ client/js/main.js
@@ -0,0 +1,29 @@
+const BLOC_L = {
+    cells: [
+        [true, false,false],
+        [true,true,true]
+    ]
+};
+const BLOC_T = {
+    cells: [
+        [true,true,true],
+        [false,true,false]
+    ]
+};
+const BLOC_I = {
+    cells:[
+        [true,true,true,true]
+    ]
+}
+const BLOC_Z = {
+    cells:[
+        [true,true,false],
+        [false,true,true]
+    ]
+}
+const BLOC_S = {
+    cells:[
+        [false,true,true],
+        [true,true,false]
+    ]
+}
